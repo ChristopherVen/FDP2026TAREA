ce450ae8ea3c85395a04e1de09d3d245eb128bd5
ChristopherVen <00110526@uca.edu.sv>
2026-05-15 11:32:58 -0600

    Se pudoo de tanto intentos

---
+++ cuentaregresiva.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int numero;
+
+    
+    cout<<"Hola"<<endl;
+    cout<<"Ingese tu numero"<<endl;
+    cin>>numero;
+    while (numero > 0)
+    {
+        cout<<"cuenta regresiva:"<<numero<<endl;
+        numero--;
+    }
+    
+
+
+    return 0;
+}
